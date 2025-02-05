bc741419270e1e324ec3f4f9f358078f7d4eda97
Tony <tonyjays@yahoo.com>
2025-02-05 06:58:42 +0100

    I changed Navigate to useNavigate in App.js file

--- src/App.js
+++ src/App.js
@@ -36,12 +36,12 @@ import UserEditScreen from './screens/UserEditScreen';
 import MapScreen from './screens/MapScreen';
 import ForgetPasswordScreen from './screens/ForgetPasswordScreen';
 import ResetPasswordScreen from './screens/ResetPasswordScreen';
-import { Navigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 function App() {
   const { state, dispatch: ctxDispatch } = useContext(Store);
   const { fullBox, cart, userInfo } = state;
-  const navigate = Navigate();
+  const navigate = useNavigate();
 
   const signoutHandler = () => {
     ctxDispatch({ type: 'USER_SIGNOUT' });
